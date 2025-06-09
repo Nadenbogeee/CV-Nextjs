7b13e44c745ed3d6e7cb791a352d0dfa2ee8866b
Yoga_Naden <143375989+Nadenbogeee@users.noreply.github.com>
2025-06-09 12:00:58 +0700

    feat: Update title and description to reflect fullstack capabilities, add icons
    This commit updates the site metadata to accurately reflect my skills as a fullstack developer and adds icons for improved visual representation.
    
    - Updates the title from "Front-end Developer" to "Fullstack Developer".
    - Modifies the description to highlight fullstack capabilities.
    - Adds icon configurations for web, apple touch, and android chrome.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,23 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Yoga Naden - Front-end Developer",
+  title: "Yoga Naden - Fullstack Developer",
   description: "Junior Developer with hands-on experience in UI and System Design using modern web technologies including React, Angular, TypeScript, and more.",
   keywords:
     "frontend developer, react developer, angular developer, typescript, javascript, web developer, ui developer, fullstack, yoganaden, yoga, naden, yoga-naden, yoga naden, naidu, reactjs, freelance,  freelance developer, freelance web developer, freelance react developer, freelance angular developer, freelance typescript developer, freelance javascript developer, freelance web developer, freelance ui developer, freelance fullstack developer, freelance reactjs developer, freelance angularjs developer, freelance typescriptjs developer, freelance javascriptjs developer, freelance webjs developer, freelance uijs developer, freelance fullstackjs developer, freelance reactjsjs developer, freelance angularjsjs developer, freelance typescriptjsjs developer, freelance javascriptjsjs developer, freelance webjsjs developer, freelance uijsjs developer, freelance fullstackjsjs developer",
   authors: [{ name: "Yoga Naden" }],
+  icons: {
+    icon: [
+      { url: "/icon-web.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-web.png", sizes: "16x16", type: "image/png" },
+      { url: "/icon-web.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+    other: [{ url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" }],
+  },
   openGraph: {
-    title: "Yoga Naden - Front-end Developer",
-    description: "Junior Developer with hands-on experience in UI and System Design using modern web technologies.",
+    title: "Yoga Naden - Fullstack Developer",
+    description: "Junior Fullstack Developer with hands-on experience in UI and System Design using modern web technologies.",
     type: "website",
   },
 };
